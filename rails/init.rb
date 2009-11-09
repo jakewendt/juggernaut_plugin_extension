af165d7cf353b5f3084d038bda3b8adeaf592bb1
Jake <github@jake.otherinbox.com>
2009-11-08 22:02:09 -0700

    Added TestRequest.session_options for testing as the juggernaut helper wasn't testing.

--- rails/init.rb
+++ rails/init.rb
@@ -2,3 +2,14 @@ require 'juggernaut_config'
 require 'juggernaut_start_stop'
 
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jqjuggernaut => [ 'juggernaut/json', 'juggernaut/juggernaut', 'juggernaut/jquerynaut', 'juggernaut/swfobject' ]
+
+#	This is required when testing and using juggernaut.  The helper calls
+#		request.session_options[:id]
+#	In development and production request is a Request
+#	but in test it is a TestRequest which does not seem to respond to 
+#	session_options even though it should, so ...
+ActionController::TestRequest.class_eval do
+        def session_options
+                { :id => '12345' }
+        end
+end if RAILS_ENV == 'test'
