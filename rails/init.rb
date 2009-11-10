cc9146a428e61b373c4e8f1182b09df6a56a9b20
Jake <github@jake.otherinbox.com>
2009-11-10 15:16:56 -0700

    Added send_to_client_on_channel to Juggernaut.

--- rails/init.rb
+++ rails/init.rb
@@ -3,6 +3,15 @@ require 'juggernaut_start_stop'
 
 ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jqjuggernaut => [ 'juggernaut/json', 'juggernaut/juggernaut', 'juggernaut/jquerynaut', 'juggernaut/swfobject' ]
 
+Juggernaut.module_eval do
+	class << self
+		#	The missing method.  The juggernaut_plugin documentation says this exists,
+		#	but it doesn't.  No big deal since its just an alias as are several others.
+		#	Nevertheless, now it works.
+		alias send_to_client_on_channel send_to_clients_on_channels
+	end
+end
+
 #	This is required when testing and using juggernaut.  The helper calls
 #		request.session_options[:id]
 #	In development and production request is a Request
